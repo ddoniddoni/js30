e0194d36de6af5bcfe67ee64dc3b0f0134710f4e
ddoniddoni <psdkei@naver.com>
2020-05-09 15:51:42 +0900

    Set Slide in on Scroll

---
+++ 13 - Slide in on Scroll/Slide.js
@@ -0,0 +1,15 @@
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = nul;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context.args);
+  };
+}
