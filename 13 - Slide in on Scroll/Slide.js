9799fe165c8b1f1e0cb73a3d3c8c61191a029ae9
ddoniddoni <psdkei@naver.com>
2020-05-10 17:11:36 +0900

    Update Slide In on Scroll

--- 13 - Slide in on Scroll/Slide.js
+++ 13 - Slide in on Scroll/Slide.js
@@ -4,7 +4,7 @@ function debounce(func, wait = 20, immediate = true) {
     let context = this,
       args = arguments;
     let later = function () {
-      timeout = nul;
+      timeout = null;
       if (!immediate) func.apply(context, args);
     };
     let callNow = immediate && !timeout;
@@ -13,3 +13,21 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context.args);
   };
 }
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+  sliderImages.forEach((sliderImage) => {
+    const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
