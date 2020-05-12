0751da0d153a3d06926356c3a9517f2ba05f7f47
ddoniddoni <psdkei@naver.com>
2020-05-12 19:56:30 +0900

    Update Number Clock

--- 98 - Number Clock/numClock.js
+++ 98 - Number Clock/numClock.js
@@ -1,15 +1,23 @@
-const numHours = document.querySelector(".hours");
-const numMinutes = document.querySelector(".minutes");
-const numSeconds = document.querySelector(".seconds");
+let clock = document.querySelector(".clock");
+let wrapper = document.querySelector(".wrapper");
 
 function getTime() {
   const now = new Date();
-  numHours.innerHTML = now.getHours();
-  numMinutes.innerHTML = now.getMinutes();
-  numSeconds.innerHTML = now.getSeconds();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  if (hours === 19) {
+    wrapper.style.background = "red";
+  }
+
+  clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
+  getTime();
   setInterval(getTime, 1000);
 }
 
