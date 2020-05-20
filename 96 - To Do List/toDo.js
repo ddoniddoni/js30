b0bf4d07db247e01170d82e96b0f498e76e2377e
ddoniddoni <psdkei@naver.com>
2020-05-20 16:49:53 +0900

    Set Image Quiz

--- 96 - To Do List/toDo.js
+++
@@ -1,7 +0,0 @@
-const plusBtn = document.querySelector(".list__plus-btn");
-
-function toDoPlus() {
-  window.open("plus.html", "", "width=400, height=400, left=600 top=200");
-}
-
-plusBtn.addEventListener("click", toDoPlus);
