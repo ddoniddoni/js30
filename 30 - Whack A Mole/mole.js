9940a97df95e12e98a4342494acb0b091ca60450
ddoniddoni <psdkei@naver.com>
2020-05-28 15:49:10 +0900

    Set Whack A Mole!

---
+++ 30 - Whack A Mole/mole.js
@@ -0,0 +1,21 @@
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    console.log("An nah thats the same one bud");
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
