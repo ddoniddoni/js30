a5adc53ad9f0dbd87d3ec6ccb0996b3a52576c28
ddoniddoni <psdkei@naver.com>
2020-05-28 15:59:22 +0900

    Update Whack a Mole

--- 30 - Whack A Mole/mole.js
+++ 30 - Whack A Mole/mole.js
@@ -19,3 +19,30 @@ function randomHole(holes) {
   lastHole = hole;
   return hole;
 }
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
