e16e0e214e06766c236cddd83851c3ce37d6b2a4
ddoniddoni <psdkei@naver.com>
2020-05-30 04:00:16 +0900

    Add Talk Movies

--- 95 - Talk Movies/movie.js
+++ 95 - Talk Movies/movie.js
@@ -1,3 +1,4 @@
+const container = document.querySelector(".container");
 const MOVIE_DATA =
   "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?";
 const API_KEY = "953230af24093ce531d0c6aef4dec491";
@@ -9,10 +10,19 @@ function getMovie() {
       return response.json();
     })
     .then((json) => {
-      const movieName = document.querySelector(".movie__name");
-      const movieOpen = document.querySelector(".movie__open");
-      movieName.textContent = json.movieListResult.movieList[0].movieNm;
-      movieOpen.textContent = json.movieListResult.movieList[0].openDt;
+      for (let i = 0; i < json.movieListResult.movieList.length; i++) {
+        const movieName = document.createElement("div");
+        const movieOpen = document.createElement("div");
+        movieName.classList = "movie__name";
+        movieOpen.classList = "movie__open";
+        const movieN = json.movieListResult.movieList[i].movieNm;
+        const movieO = json.movieListResult.movieList[i].openDt;
+        movieName.textContent = movieN;
+        movieOpen.textContent = movieO;
+        container.appendChild(movieName);
+        container.appendChild(movieOpen);
+        console.log(movieN);
+      }
     });
 }
 
