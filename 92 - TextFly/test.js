cc93a0710d0226020b7829b79ad8cc167b9ee5e9
ddoniddoni <psdkei@naver.com>
2020-07-15 17:33:02 +0900

    Test TextFly

--- 92 - TextFly/test.js
+++ 92 - TextFly/test.js
@@ -1,68 +1,74 @@
 const input_text = document.querySelector(".input__text");
-const input_btn = document.querySelector(".input__btn");
-
-const words = {};
-const words_attr = [];
+const input_btn = document.querySelector(".text__btn");
+let lyric = "안 녕 하 세 요 반 갑 습 니 다 또 만 나 요";
+let words = {};
+let words_attr = [];
 const canvas = document.querySelector(".canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+string_handle(lyric);
 
-const ctx = canvas.getContext("2d");
-
-const onClick = () => {
-  let text1 = input_text.value;
-  const viewText = text1;
-  console.log(viewText);
+const onClick = (e) => {
+  e.preventDefault();
+  console.log(input_text.value);
+  lyric.concat(" ", input_text.value);
+  console.log(lyric);
   input_text.value = "";
 };
 
+input_btn.addEventListener("click", onClick);
 if (canvas.getContext) {
-  const ctx = canvas.getContext("2d"),
-    ctx_width = canvas.width,
-    ctx_height = canvas.height;
-  console.log(ctx_width, ctx_height);
-  ctx.fillStyle = "White";
-  ctx.lineWidth = 5;
+  const c = canvas.getContext("2d"),
+    w = canvas.width,
+    h = canvas.height;
+  console.log(w, h);
+  c.strokeStyle = "red";
+  c.fillStyle = "white";
+  c.lineWidth = 5;
 
+  // constructor
   Word = function (key) {
     this.text = key;
     this.x = Math.random() * w;
     this.y = Math.random() * h;
     this.font = `${words[key] * 20}px arial`;
+    // this.font = words[key] * 10 + "px arial";
     this.speed = words[key];
   };
   for (key in words) {
     words_attr.push(new Word(key));
   }
+
   console.log(words_attr.length);
 
-  const animation = () => {
+  function animation() {
     for (let i = 0; i < words_attr.length; i++) {
-      ctx.font = words_attr[i].font;
-      ctx.fillText(words_attr[i].text, words_attr[i].x, words_attr[i].y);
-      words_attr[i].width = ctx.measureText(words_attr[i].text).width;
-      ctx.stroke();
+      c.font = words_attr[i].font;
+      c.fillText(words_attr[i].text, words_attr[i].x, words_attr[i].y);
+      words_attr[i].width = c.measureText(words_attr[i].text).width;
+      c.stroke();
     }
     move();
-  };
+  }
 
-  const move = () => {
+  function move() {
     for (let i = 0; i < words_attr.length; i++) {
-      if (words_attr[i].x > ctx_width) {
+      if (words_attr[i].x > w) {
         words_attr[i].x = -words_attr[i].width;
-        words_attr[i].y = Math.random() * ctx_height;
+        words_attr[i].y = Math.random() * h;
       } else {
         words_attr[i].x += words_attr[i].speed;
       }
     }
-  };
-  setInterval(() => {
-    ctx.clearRect(0, 0, ctx_width, ctx_height);
+  }
+
+  setInterval(function () {
+    c.clearRect(0, 0, w, h);
     animation();
   }, 24);
 }
 
-const handleString = (str) => {
+function string_handle(str) {
   const split_str = str.split(" ");
   const word_array = [];
   const word_count = [];
@@ -84,6 +90,4 @@ const handleString = (str) => {
     words[word_array[i]] = word_count[i];
   }
   return words;
-};
-
-input_btn.addEventListener("click", onClick);
+}
